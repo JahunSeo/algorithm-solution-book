c8b2b3078bad71860a8be6e73ec3cd09943c2cc9
JahunSeo <wkgjs0809@gmail.com>
2021-08-19 23:16:56 +0900

    [johnny] 2261 updated

--- beakjoon/2261_가장_가까운_두_점.py
+++ beakjoon/2261_가장_가까운_두_점.py
@@ -1,13 +1,11 @@
-# https://www.acmicpc.net/problem/2261
-
 def get_dist(p1, p2):
     dx, dy = p1[0] - p2[0], p1[1] - p2[1]
     return  dx * dx + dy * dy
 
 def bruteforce(start, end):
-    global P, Q
+    global P
     min_dist = float("inf")
-    for i in range(start, end+1):
+    for i in range(start, end):
         for j in range(i+1, end+1):
             dist = get_dist(P[i], P[j])
             if dist < min_dist:
@@ -15,7 +13,7 @@ def bruteforce(start, end):
     return min_dist
 
 def find_closest(start, end):
-    global P, Q
+    global P
     # 베이스 케이스: 3개보다 작으면 단순 계산
     if start >= end:
         return float("inf")
@@ -34,24 +32,33 @@ def find_closest(start, end):
     #  - 이 때, y 좌표를 기준으로 이미 정렬되어 있는 Q에서 찾아
     #  - strip을 y를 기준으로 졍렬된 상태로 만듦
     strip = []    
-    for point in Q:
+    for i in range(mid, start-1, -1):
+        point = P[i]
         dx = point[0] - mid_point[0]
         if dx * dx < d:
             strip.append(point)
+        else:
+            break
+    for j in range(mid+1, end+1):
+        point = P[j]
+        dx = point[0] - mid_point[0]
+        if dx * dx < d:
+            strip.append(point)
+        else:
+            break
+    strip.sort(key=lambda p: p[1])        
 
     # 3단계: strip 중에서 최소 거리 찾기
     #  - y를 기준으로 정렬된 상태이므로, y 좌표 거리가 d 이상인 경우 탐색 대상에서 제외
     min_dist = d
-    for i in range(len(strip)):
+    for i in range(len(strip)-1):
         p1 = strip[i]
         for j in range(i+1, len(strip)):
             p2 = strip[j]
             # 두 점 간 y 좌표의 거리가 d 이상인 경우, 다음 p1으로 이동 
             # (비교 시, 거리는 제곱되어 있으므로 y좌표 차이도 제곱 필요)
             dy = p2[1] - p1[1]
-            print(p1, p2, dy)
-
-            if dy * dy >= d: 
+            if dy * dy >= min_dist: 
                 break
             dist = get_dist(p1, p2)
             if dist < min_dist:
@@ -60,35 +67,15 @@ def find_closest(start, end):
     return min_dist
 
 
-points = ["0 0", "10 10", "0 10", "10 0"]
-points = ["0 0",
-"2 1",
-"4 1",
-"6 2"]
-points = [
-"-5 0",
-"-3 4",
-"2 1",
-"3 4",
-"-1 1",
-"8 8",
-"1 7",
-]
-points = [tuple(map(int, p.split())) for p in points]
-# print(points)
+import sys
+N = int(sys.stdin.readline())
+points = []
+for _ in range(N):
+    point = tuple(map(int, sys.stdin.readline().split()))
+    points.append(point)
 
-
-
-P = sorted(points, key=lambda p: p[0])
-Q = sorted(points, key=lambda p: p[1])
-
-
-print(P)
-print(Q)
-
-# print(bruteforce(0, 1))
-
-
-print(find_closest(0, len(P)-1))
-
-
+if len(points) != len(set(points)):
+    print(0)
+else:
+    P = sorted(points, key=lambda p: p[0])
+    print(find_closest(0, len(P)-1))
