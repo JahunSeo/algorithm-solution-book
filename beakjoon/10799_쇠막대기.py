adaf8cc60ab15bd10c83af2d9c64029874892d4d
JahunSeo <wkgjs0809@gmail.com>
2021-08-19 12:38:20 +0900

    [johnny] 10799 update

--- beakjoon/10799_쇠막대기.py
+++ beakjoon/10799_쇠막대기.py
@@ -1,13 +1,15 @@
 questions = "()(((()())(())()))(())"
-questions = "(()())"
-questions = "((()()))" # 반례!
+# questions = "(()())"
+# questions = "((()()))" # 반례!
 print("questions", questions)
 
 stack = []
 answer = 0
+sub_count = 0
 for q in questions:
     # 시작 괄호인 경우 스택에 추가
     if q == "(":
+        sub_count += 1
         stack.append(q)
         continue
     # 끝 괄호인 경우
@@ -15,7 +17,8 @@ for q in questions:
     if stack[-1] == "(":
         # 스택에서 레이저를 뺀 뒤, 남은 시작 괄호의 수를 센다
         stack.pop()
-        answer += sum((c == "(" for c in stack))
+        sub_count -= 1
+        answer += sub_count # sum((c == "(" for c in stack))
         # 스택에 L을 추가한다
         stack.append("L")
         continue
@@ -24,8 +27,10 @@ for q in questions:
         while stack:
             prev = stack.pop()
             if prev == "(":
-                # 괄호의 
+                # 막대 끝단을 개수에 추가
+                sub_count -= 1
                 answer += 1
+                # 다시 막대임을 표시해줌
                 stack.append("L")
                 break
 
---
+++ beakjoon/2014_소수의곱.py
@@ -0,0 +1,7 @@
+K, N = 4, 19
+values = [2,3,5,7]
+
+import heapq
+
+arr = []
+    
