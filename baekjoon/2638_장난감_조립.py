16a35973c404d18f21f30e0e9aa64f75061ac64c
JahunSeo <wkgjs0809@gmail.com>
2021-08-21 16:38:21 +0900

    [johnny] 2638 memory overflow

--- baekjoon/2638_장난감_조립.py
+++ baekjoon/2638_장난감_조립.py
@@ -8,14 +8,13 @@ M = int(sys.stdin.readline())
 
 # 1단계: 각 부품 별로 필요한 하위 부품을 파악
 # - 각 상위 부품을 만드는 데 필요한 하위 부품을 필요한 개수 만큼 배열에 저장
-# - 가령, {상위: [하위1, 하위1, 하위2, 하위2, 하위2, 하위3 ]}
+# - 가령, {상위: [(하위1, 2), (하위2, 3), (하위3, 1)]}
 # - 이 때, adj에 key로 등록되지 않은 부품은 더 이상 분리될 수 없는 기본 부품을 의미
 adj = defaultdict(list)
 for _ in range(M):
     x, y, k = tuple(map(int, sys.stdin.readline().split()))
     # x를 만드는데 하위 부품 y가 필요한 개수만큼 인접 리스트에 추가
-    for _ in range(k):
-        adj[x].append(y)
+    adj[x].append((y, k))
 
 # 누적된 기본 부품을 세는 dict 초기화
 subitems = defaultdict(int)
@@ -23,16 +22,19 @@ subitems = defaultdict(int)
 s = N
 frontier = deque(adj[s])
 while frontier:
-    v1 = frontier.popleft()
+    # 하위 부품 v1, 필요한 개수 k1
+    v1, k1 = frontier.popleft()
     # v1이 인접 리스트에 없는 경우, 기본 부품임으로 더 이상 분리 불가
     if v1 not in adj:
         # 기본 부품 개수에 누적 후 다음 아이템으로 넘어감
-        subitems[v1] += 1
+        subitems[v1] += k1
         continue
     # v1이 인접 리스트에 있는 경우, 하위 부품으로 분리해주어야 함
     # v1의 모든 하위 부품을 frontier에 추가
-    for v2 in adj[v1]:
-        frontier.append(v2)
+    for v2, k2 in adj[v1]:
+        # 이 때, v1을 1개 만들기 위해 v2가 k2개 필요하므로,
+        # v1을 k1개 만들기 위해서는 v2가 k1*k2개 필요
+        frontier.append((v2, k1*k2))
 
 # 누적된 기본 부품 개수 출력
 for i in range(1, N+1):
