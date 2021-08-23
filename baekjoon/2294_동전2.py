9dbeeeb8286f9f5be27cf085ba9ab891987d81b4
JahunSeo <wkgjs0809@gmail.com>
2021-08-23 09:59:55 +0900

    [johnny] 2294 updated

--- baekjoon/2294_동전2.py
+++ baekjoon/2294_동전2.py
@@ -9,25 +9,21 @@ for _ in range(N):
 # 작은 값부터 확인 가능하도록 오름차순 정렬
 coins.sort()
 
-# 사용 동전의 최대 개수 초기 설정
-min_count = K 
+# 초기값 설정
 # 동전들 중 목표 금액과 일치하는 값이 있는지 확인하여 조기 종료
 if K in coins:
     min_count = 1
     frontier = []
 else:
-    # (목표 금액, 누적 동전 수)
+    min_count = -1
     frontier = deque([(K, 0)])
 
-# 동전 분할하기
-# 분할 성공 여부 초기화
-success = False
-# 기록 누적하기
+# 하위 문제로 분할 및 기록 누적하기
 # - 너비 우선 탐색 과정에서 이미 지나간 숫자가 나중에 다시 등장하는 경우,
 # - 해당 숫자까지 도달하는데까지의 누적 동전 수는 크고 남은 경로는 동일할 것이기 때문에
 # - 결국 누적 동전 수가 최소가 될 수 없으며, 추가 탐색할 필요가 없음
 parent = {K: None}
-while frontier:
+while min_count < 0 and frontier:
     # 남은 금액 v1, 누적 동전 수 cnt1
     v1, cnt1 = frontier.popleft()
     # 사용 가능한 동전들 하나씩 확인
@@ -43,8 +39,7 @@ while frontier:
             break
         # 남은 금액이 0인 경우, 조합 완성이며 다음 동전은 음수가 나올 것이므로 종료
         elif v2 == 0:
-            success = True
-            min_count = min(min_count, cnt2)
+            min_count = cnt2
             break
         # 남은 금액이 양수인 경우, 추가 분할을 위해 큐에 추가
         else:
@@ -52,7 +47,4 @@ while frontier:
             parent[v2] = v1
             frontier.append((v2, cnt2))
 
-if success:
-    print(min_count)
-else:
-    print(-1)
+print(min_count)
