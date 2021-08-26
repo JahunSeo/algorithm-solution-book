d3a874eb31c3e28ef9fcb9c20e4d20802debbcef
JahunSeo <wkgjs0809@gmail.com>
2021-08-26 11:46:32 +0900

    [johnny] 1697 solved

--- baekjoon/1697_숨바꼭질.py
+++ baekjoon/1697_숨바꼭질.py
@@ -10,30 +10,31 @@ def BFS(s):
     while frontier:
         v1, time1 = frontier.popleft()
         # 한 노드에서 이동할 수 있는 경우 3가지
-        for v2 in [v1*2,v1+1,v1-1]:
+        next_nodes = [v1+1,v1-1]
+        if v1 % 2 == 0:
+            next_nodes.append(v1//2)
+        for v2 in next_nodes:
             time2 = time1+1
             # 이동 불가능한 지점인지 확인
-            if v1 <=0:
+            if v2 <0:
                 continue
             # 이미 방문한 위치인지 확인
             if v2 in parent:
                 continue
             # 새로운 위치인 경우, 우선 목표 지점인지 확인
-            if v2 == K:
-                print("find", v2, v1)
+            if v2 == N:
                 return time2
             # 새로운 위치이지만 목표 지점이 아닌 경우
             # 새로운 탐색 대상인지 판단
-            # if v2 > K*2:
-            #     continue
+            if v2 < N//2:
+                continue
             # 새로운 탐색 대상으로 판단될 경우
-            print("new", v2, v1, frontier)
             parent.add(v2)
             frontier.append((v2, time2))
     return False
 
-s = N
-if s == K:
+s = K
+if s == N:
     print(0)
 else:
     time = BFS(s)
