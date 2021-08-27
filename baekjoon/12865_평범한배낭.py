b99ead20fa2ad2adb78156ba84bad5d8e42966bd
JahunSeo <wkgjs0809@gmail.com>
2021-08-27 19:49:12 +0900

    [johnny] 12865 solved

--- baekjoon/12865_평범한배낭.py
+++ baekjoon/12865_평범한배낭.py
@@ -9,22 +9,44 @@ for _ in range(N):
     w, value = tuple(map(int, sys.stdin.readline().split()))
     items.append((w, value))
 items.sort()
-print(items)
+# print(items)
 
 # 만들 수 있는 전체 경우의 수는 2^N
 # nC0 + nC1 + nC2 + ... + nCn
 
-candidates = [(0,0)]
-max_value = 0
+
+# 추상화 1단계: 답이 될 수 있는 조합들을 모두 남긴다
+# candidates = [(0,0)]
+# max_value = 0
+# for w1, value1 in items:
+#     new_candidates = candidates[:]
+#     for w2, value2 in candidates:
+#         new_w = w1 + w2
+#         new_value = value1 + value2
+#         if new_w <= K:
+#             new_candidates.append((new_w, new_value))
+#             max_value = max(max_value, new_value)
+#     candidates = new_candidates
+#     print((w1, value1), K, max_value, new_candidates)
+
+# print(max_value)
+
+# 추상화 2단계: 답이 될 수 있는 조합들에서 동일 무게 중 가치가 최대인 조합만 남긴다
+# - 동일 무게 중 가치가 작은 조합은, 동일 무게 중 가치가 큰 조합보다 더 좋은 조합을 만들 수 없음
+# candidates는 각 무게에서 최대의 가치를 저장
+# - 각 위치에 무게가 k이고 가치가 0인 아이템이 있었다고 생각
+candidates = [0] * (K+1)
 for w1, value1 in items:
+    # print("start!", w1, value1, candidates)
     new_candidates = candidates[:]
-    for w2, value2 in candidates:
+    for w2, value2 in enumerate(candidates):
         new_w = w1 + w2
         new_value = value1 + value2
-        if new_w <= K:
-            new_candidates.append((new_w, new_value))
-            max_value = max(max_value, new_value)
+        # print(w2, value2, new_w, new_value)
+        if new_w <= K and candidates[new_w] < new_value:
+            # print("update:", new_w, new_value, candidates[new_w])
+            new_candidates[new_w] = new_value
     candidates = new_candidates
-    print((w1, value1), K, max_value, new_candidates)
+    # print("result:", (w1, value1), K, candidates)
 
-print(max_value)
+print(max(candidates))
