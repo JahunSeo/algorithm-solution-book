206e6527659cb93fa356d5d88423e58d856b40c8
JahunSeo <wkgjs0809@gmail.com>
2021-08-28 10:15:22 +0900

    [johnny] 11049 update /ing

--- baekjoon/11049_행렬곱셈순서.py
+++ baekjoon/11049_행렬곱셈순서.py
@@ -16,23 +16,22 @@ for _ in range(N):
 
 # 2단계: 상향식으로 각 행렬조합을 만들기 위한 최소 계산 횟수 구하기
 # diag은 c - r 이며, r == c 인 구간은 이미 0으로 설정되어 있으므로, 1부터 시작
-for diag in range(0, N):
+for diag in range(1, N):
     print("diag!", diag)
     for c in range(diag, N):
         r = c - diag
-        print("to calculate:", (r, c))
-        if r == c:
-            memo[r][c] = (0, *matrices[r])
-            continue
-        min_matrix = (float("inf"), None, None)
+        m1, m3 = matrices[r], matrices[c]
+        print("to calculate:", (r, c), m1, m3, m1[0]*m1[1]*m3[1])
+        min_score = float("inf")
         for i in range(r, c):
-            m1, m2 = memo[r][i], memo[i+1][c]
-            score = m1[0] + m2[0] + m1[1]*m1[2]*m2[2]
-            if score < min_matrix[0]:
-                min_matrix = (score, m1[1], m2[2])
-        print("result", min_matrix)
-        memo[r][c] = min_matrix
+            s1, s2 = memo[r][i], memo[i+1][c]
+            m2 = matrices[i]
+            score = s1 + s2 + m1[0]*m2[1]*m3[1]
+            if score < min_score:
+                min_score = score
+        print("result", min_score)
+        memo[r][c] = min_score
 
-print(memo[0][N-1][0])
+print(memo[0][N-1])
         
 
