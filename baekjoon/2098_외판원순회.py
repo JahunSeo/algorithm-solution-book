fe140cae20f908acc1bc7a35091c771c87c0cca9
JahunSeo <wkgjs0809@gmail.com>
2021-08-29 16:49:13 +0900

    [johnny] 2098 ing

--- baekjoon/2098_외판원순회.py
+++ baekjoon/2098_외판원순회.py
@@ -1,5 +1,3 @@
-## 잘못된 풀이: 중간에 방문했던 도시로의 방문은 제한되는데 이 점을 고려하지 못했음!
-
 import sys
 
 sys.stdin = open("./baekjoon/testcase.txt")
@@ -10,47 +8,45 @@ for _ in range(N):
     row = list(map(int, sys.stdin.readline().split()))
     costs.append(row)
 
+def print_matrix(m):
+    for r in m:
+        print(r)
+    print("")
+
 # 1단계: 부분 문제 정의
-# - fn(k,B) = k개 도시를 방문하면서 B를 마지막으로 방문했을 때의 최소 비용
-#           = min( fn(k-1,CITY) + dist(CITY,B) for CITY in {출발지 A와 도착지 B를 제외한 도시들} ) 
-# - dist(A,B) = 도시 A에서 도시 B로 이동하는 비용
-# - 부분 문제의 개수 = (n-1)**2 (이 때 n-1은 출발지를 제외한 방문해야 하는 도시의 개수)
-# - 선택지의 개수 = 출발지와 도착지를 제외한 도시들의 개수
+# - 각 시점이 처할 수 있는 상태 S(i) = i번째로 방문 가능한 도시의 수 x 남은 도시의 구성
+# - 가령, A를 출발해 {B,C,D,E}를 방문한다고 할 때
+#   1번째 도시로 B를 방문하면, 남는 도시는 {C,D,E}
+#   2번째 도시로 B를 방문하면, 남는 도시는 {D,E}, {C,E}, {C,D} 중 하나의 경우
+#                        (각각 1번째로 C,D,E를 방문한 뒤, 2번째로 B를 방문한 경우)       
+# - 결과적으로, 각 시점 i의 상태 개수 S(i)는
+#   (출발지를 제외한 도시 개수 N-1) x (본인까지 제외한 N-2개 중 남은 도시 N-1-i개를 선택하는 경우의 수) 
+# - 부분 문제의 개수 = sum(N * S(i) for i in range(1, N))
 
-# 2단계: 메모 테이블 생성
-# - 각 부분 문제의 정답을 저장하는 메모 테이블 생성
-# - 방문한 도시의 개수 1~(n-1), 마지막이 되는 도시의 개수 1~(n-1)
-# - 도시 인덱스와 맞추기 위해 방문 도시 개수, 마지막 도시 개수 모두 N으로 설정
-# - 도시 개수가 1개 일 때는 사전 입력
-memo = [[0]*(N) for _ in range(N)]
+# 2단계: 메모 테이블 구성
+# - 각 부문 문제의 정답을 저장할 메모 테이블 구성
+# - 각 시점마다 S의 개수가 달라 난해함
+# - 하지만 남은 도시의 구성은 도시 방문 여부를 0,1로 표시한 tuple로 표현 가능하고, 이를 일반화하면 2^(N-1)개임
+# - 즉, B,C,D,E 중 C,D가 남았다면 (0,1,1,0)이며 이는 0~15 중 6에 해당함
+# - 그러므로 S(i)는 (N-1) * (2^(N-1))개 중 하나의 상태로 표현 가능함
 start = 0
-# 방문 도시가 1개이고, 마지막 도시가 city일 때의 비용
-for city in range(1, N):
-    memo[1][city] = costs[start][city]
-print(memo)
+cities = [c for c in range(1, N)]
+print(cities)
+# memo[time][city_to_visit][id_of_rest_cities]
+# - 편의를 위해 0번째 시간, 0번째 도시 추가
+memo = []
+for i in range(0, N):
+    timeline = []
+    for c in range(0, N):
+        timeline.append([0] * (2**N))
+    memo.append(timeline)
 
-# 3단계: 부분 문제의 선후 관계에 따라 문제 해결
-# - 방문 도시 개수가 k개인 부분 문제는 방문 도시 개수가 k-1개일 때의 부분 문제들을 활용하므로
-# - 방문 도시 개수가 1개일 때부터 순차적으로 해결
-for k in range(2, N):
-    for city in range(1, N):
-        # 구하려는 값: k개 도시를 방문하면서 city를 마지막으로 방문했을 때의 최소 비용
-        min_score = float("inf")
-        for prev_city in range(1, N):
-            if city != prev_city:
-                # k-1개 도시를 방문하면서 prev_city를 마지막으로 방문했을 때의 최소 비용에 
-                # prev_city에서 city로 이동하는 비용을 추가
-                score = memo[k-1][prev_city] + costs[prev_city][city]
-                min_score = min(min_score, score)
-        memo[k][city] = min_score
 
-print(memo)
-# 4단계: 본래의 문제 해결
-# - n-1번째 도시를 방문하면서 각 city를 마지막으로 방문했을 때의 최소 비용에 다시 start로 돌아가는 비용 추가
-# - 그 중의 최소값을 답으로 결정
-min_score = float("inf")
-for city in range(1, N):
-    score = memo[-1][city] + costs[city][start]
-    min_score = min(min_score, score)
+# 메모는 마지막 도시 방문부터 시작
+for c in cities:
+    id_of_rest = 0 # 남은 도시가 없으므로 (A,B,C,D,E) = (0,0,0,0,0) = 0
+    memo[N-1][c][id_of_rest] = costs[c][start]
+print_matrix(memo[-1])
 
-print(min_score)
+# 3단계: 부분 문제의 선후관계에 맞게 부분 문제 해결 
+# - 이제 비트마스크를 사용해야하는 타이밍..
