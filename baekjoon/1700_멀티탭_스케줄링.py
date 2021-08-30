a2f6810805e0ce2e4265287ce5f5314b39ffae8d
JahunSeo <wkgjs0809@gmail.com>
2021-08-30 17:28:01 +0900

    [johnny] 1700 incorrect

--- baekjoon/1700_멀티탭_스케줄링.py
+++ baekjoon/1700_멀티탭_스케줄링.py
@@ -1,45 +1,36 @@
 import sys
+import heapq
+
 sys.stdin = open("./baekjoon/testcase.txt")
 N, K = tuple(map(int, sys.stdin.readline().split()))
 items = list(map(int, sys.stdin.readline().split()))
 
-current = set()
-i = -1
-count = 0
-while i < K-1:
-    i += 1
-    item = items[i]
-    print("item", i, item, current)
-    # 이미 콘센트에 꽂혀 있는 경우
-    if item in current:
+where = {}
+updated = []
+for idx in range(K):
+    item = items[idx]
+    if item in where:
+        prev_idx = where[item]
+        updated[prev_idx][1] = idx
+    where[item] = idx
+    updated.append([item, float("inf")])
+
+# print(updated)
+
+count = 0        
+current = []    
+for item, next_loc in updated:
+    # print(item, current)
+    # 이미 콘센트에 있는 경우
+    if sum(c == item for _, c in current) != 0:
         continue
-    # 콘센트가 비어 있는 경우
+    # 콘센트에 자리가 남는 경우
     if len(current) < N:
-        current.add(item)
+        heapq.heappush(current, (-next_loc, item))
         continue
-    # 콘센트에 없는 경우, current에서 뽑을 대상을 선택
-    # - current의 아이템 중, 현재 아이템 다음으로 오는 N-1개에서 없는 아이템 선택 
-    #   (현재 아이템 다음으로 오는 N-1개 중에 없으면 결국에는 뽑여야 하는 아이템이기 때문)
-    to_delete = None
-    for c in current:
-        if c not in items[i+1:i+N]:
-            to_delete = c
-            break
-        # is_coming = False
-        # for next_idx in range(i+1, i+N):
-        #     if next_idx < K and c == items[next_idx]:
-        #         is_coming = True
-        #         break
-        # if not is_coming:
-        #     to_delete = c
-        #     break
-    print("  delete", items[i:i+N], current, to_delete)
-    current.remove(to_delete)
-    current.add(item)
+    # 콘센트에서 뽑아야 하는 경우
+    heapq.heappop(current)
+    heapq.heappush(current, (-next_loc, item))
     count += 1
 
-print(current)
 print(count)
-
-
-    
