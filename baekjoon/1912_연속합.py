faf69e1334e9ccd206c66de64e37d5910ee228ba
JahunSeo <wkgjs0809@gmail.com>
2021-09-01 23:49:51 +0900

    [johnny] 1912 timeout2

--- baekjoon/1912_연속합.py
+++ baekjoon/1912_연속합.py
@@ -4,15 +4,30 @@ sys.stdin = open("./baekjoon/testcase.txt")
 N = int(sys.stdin.readline())
 values = list(map(int, sys.stdin.readline().split()))
 
-print(N, values)
+# print(N, values)
 
-# [반례] 모든 값이 음수가 나오는 상황 주의하기
-max_value = float("-inf")
 
-for i in range(N):
-    for prev in range(0, i+1):
-        if i != prev:
-            values[prev] = values[prev] + values[i]
-        max_value = max(max_value, values[prev])
+memo = {}
+def solve(i, j):
+    if i==j:
+        return values[i]
+    k = (i+j)//2
+    max_left, left = 0, 0
+    for p in range(k, -1, -1):
+        left += values[p]
+        max_left = max(max_left, left)
+    max_right, right = 0, 0
+    for q in range(k+1, len(values)):
+        right += values[q]
+        max_right = max(max_right, right)
+    
+    first = solve(i, k)
+    second = solve(k+1, j)
+    third = max_left + max_right
+    
+    memo[(i, j)] = max(first, second, third)
+    # print((i, j), memo[(i,j)], first, second, third)
 
-print(max_value)
+    return memo[(i, j)]
+
+print(solve(0, len(values)-1))
