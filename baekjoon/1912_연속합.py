a5d16cb4ec709bf326ae0f46c155e1bcd7693f77
JahunSeo <wkgjs0809@gmail.com>
2021-09-01 23:22:47 +0900

    [johnny] 1912 timeout

--- baekjoon/1912_연속합.py
+++ baekjoon/1912_연속합.py
@@ -7,17 +7,12 @@ values = list(map(int, sys.stdin.readline().split()))
 print(N, values)
 
 # [반례] 모든 값이 음수가 나오는 상황 주의하기
-memo = [[0] * N for _ in range(N)]
-# 길이가 1인 상황 먼저 처리
 max_value = float("-inf")
-for i in range(N):
-    memo[i][i] = values[i]
-    max_value = max(max_value, memo[i][i])
 
-for diag in range(1, N):
-    for c in range(diag, N):
-        r = c - diag
-        memo[r][c] = memo[r][c-1] + memo[c][c]
-        max_value = max(max_value, memo[r][c])
+for i in range(N):
+    for prev in range(0, i+1):
+        if i != prev:
+            values[prev] = values[prev] + values[i]
+        max_value = max(max_value, values[prev])
 
 print(max_value)
