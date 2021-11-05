01dec35b02c151eb9a0156c48c05203ffd379b7a
JahunSeo <wkgjs0809@gmail.com>
2021-11-05 09:45:41 +0900

    [johnny] solve baekjoon 2447

--- baekjoon/2447.py
+++ baekjoon/2447.py
@@ -1,18 +1,29 @@
 import sys
 n = int(sys.stdin.readline())
 
-def toggle(r, c, width):
-    # print(r, c, width)
-    sub = width // 3
-    if r >= sub and r < sub*2 and c >= sub and c < sub*2:
-        return " "
-    elif sub == 1:
-        return "*"
-    else:
-        return toggle(r%sub, c%sub, sub)
+answer = [""] * n
 
-for i in range(n):
-    row = ""
-    for j in range(n):
-        row += toggle(i, j, n)
+def blank(r, c, width):
+    for i in range(width):
+        for j in range(width):
+            answer[r+i] += " "
+
+def fill(r, c, width):
+    sub = width // 3
+    if width == 1:
+        answer[r] += "*"
+        return
+    fill(r, c, sub)
+    fill(r, c+sub, sub)
+    fill(r, c+sub*2, sub)
+    fill(r+sub, c, sub)
+    blank(r+sub, c+sub, sub)
+    fill(r+sub, c+sub*2, sub)
+    fill(r+sub*2, c, sub)
+    fill(r+sub*2, c+sub, sub)
+    fill(r+sub*2, c+sub*2, sub)
+
+fill(0, 0, n)
+
+for row in answer:
     print(row)
